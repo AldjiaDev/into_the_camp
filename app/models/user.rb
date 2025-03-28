9723e812492ac5b508e148974f6929ba8da6275b
Aldjia Boughias <boughias-aldjia@live.fr>
2025-03-28 14:26:59 +0100

    Add users controller, gemdevise and layouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_secure_password
   validates :email, presence: true, uniqueness: true
 
-  has_many :products
+  has_many :products, dependent: :destroy
   has_many :reservations
   has_many :reviews
 end
